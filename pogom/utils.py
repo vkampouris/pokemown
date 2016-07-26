5bbaeea3e2f796a5f6bf114c2cfa72358f385a9a
vkampouris <vkampouris@live.com>
2016-07-26 04:02:42 +0300

    Revert "Dynos for Heroku"
    This reverts commit 41d502d53c1e28df778152173110ce06237407d2.

--- pogom/utils.py
+++ pogom/utils.py
@@ -80,13 +80,12 @@ def get_args():
 
 def get_random_credentials():
 
-    Config = get_config()
-    
-	usernames = Config.get("Authentication", "Usernames").split(",")
+    usernames = Config.get("Authentication", "Usernames").split(",")
     passwords = Config.get("Authentication", "Passwords").split(",")
 
     index = randint(0, len(usernames) - 1)
 
+    Config = get_config()
     creds = [usernames[index], passwords[index]]
 
     return creds
